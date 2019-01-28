b02562771317b970729adeda4de1e66ea10415ba
smelge <traildogfilms@gmail.com>
2019-01-28 20:12:45 +0000

    Open develop branch

--- src/App.js
+++ src/App.js
@@ -38,8 +38,16 @@ class App extends Component {
                     <li className="nav-item active">
                       <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="/directory">Trail Directory</a>
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Trail Directory
+                      </a>
+                      <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="/">Option 1</a>
+                        <a className="dropdown-item" href="/">Option 2</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="/">Option 3</a>
+                      </div>
                     </li>
                     <li className="nav-item">
                       <a className="nav-link" href="/login">Login</a>
